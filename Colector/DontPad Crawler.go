582e5095710e054dbb9556c34adb2672dd403dc0
Felipe <felipeg.bueno@outlook.com>
2022-06-21 13:46:58 -0300

    Add config file and directories

--- Colector/DontPad Crawler.go
+++ Colector/DontPad Crawler.go
@@ -2,41 +2,43 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"time"
 
 	"github.com/go-resty/resty/v2"
+	"gopkg.in/yaml.v3"
 )
 
 const (
 	URL_BASE   = "http://dontpad.com/"
 	URL_SEARCH = URL_BASE + "{name}"
+	FILE_PATH  = "CrawlerResults/"
 )
 
-func crawler(keyword string) (response string, err error) {
-	cli := resty.New()
-	resp, err := cli.R().SetPathParam("name", keyword).Get(URL_SEARCH)
-	if err != nil {
-		return
-	}
-	response = string(resp.Body())
-	return response, nil
+type configuration struct {
+	Keywords []string `yaml:"keywords"`
 }
 
 func main() {
-	keywords := []string{
-		"test",
-		"mydontpad",
+	var config configuration
+	yamlFile, err := ioutil.ReadFile("config/config.yaml")
+	if err != nil {
+		log.Println(err)
 	}
-	for _, keyword := range keywords {
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Println(err)
+	}
+	for _, keyword := range config.Keywords {
 		result, err := crawler(keyword)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 		currentTime := time.Now()
-		fileName := fmt.Sprintf("%v-%v.html", keyword, currentTime.Format("2006-01-02-15-04-05"))
+		fileName := fmt.Sprintf("%v%v-%v.html", FILE_PATH, keyword, currentTime.Format("2006-01-02-15-04-05"))
 		f, err := os.OpenFile(fileName, os.O_CREATE, 0766)
 		if err != nil {
 			log.Println(err)
@@ -47,4 +49,14 @@ func main() {
 			log.Println(err)
 		}
 	}
+}
+
+func crawler(keyword string) (response string, err error) {
+	cli := resty.New()
+	resp, err := cli.R().SetPathParam("name", keyword).Get(URL_SEARCH)
+	if err != nil {
+		return
+	}
+	response = string(resp.Body())
+	return response, nil
 }
--- Parser/DontPad Parser.go
+++ Parser/DontPad Parser.go
@@ -11,27 +11,13 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
-func parser(fileName string) {
-	f, e := os.Open(fileName)
-	if e != nil {
-		log.Fatal(e)
-	}
-	defer f.Close()
-	doc, err := goquery.NewDocumentFromReader(f)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	doc.Find("#text").Each(func(i int, s *goquery.Selection) {
-		inside_html, _ := s.Html()
-		decodedHtml := html.UnescapeString(inside_html)
-		fmt.Printf(decodedHtml)
-
-	})
-}
+const (
+	CRAWLER_FILE_PATH = "CrawlerResults/"
+	PARSED_FILE_PATH  = "Parsed Files/"
+)
 
 func main() {
-	files, err := ioutil.ReadDir(".")
+	files, err := ioutil.ReadDir(CRAWLER_FILE_PATH)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -40,6 +26,34 @@ func main() {
 		if file.IsDir() || !strings.Contains(file.Name(), ".html") {
 			continue
 		}
-		parser(file.Name())
+		err = parser(file.Name())
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		err = os.Rename(CRAWLER_FILE_PATH+file.Name(), PARSED_FILE_PATH+file.Name())
+		if err != nil {
+			log.Print(err)
+		}
 	}
+}
+
+func parser(fileName string) error {
+	f, e := os.Open(CRAWLER_FILE_PATH + fileName)
+	if e != nil {
+		return e
+	}
+	defer f.Close()
+	doc, err := goquery.NewDocumentFromReader(f)
+	if err != nil {
+		return err
+	}
+
+	doc.Find("#text").Each(func(i int, s *goquery.Selection) {
+		inside_html, _ := s.Html()
+		decodedHtml := html.UnescapeString(inside_html)
+		fmt.Printf(decodedHtml)
+
+	})
+	return nil
 }
